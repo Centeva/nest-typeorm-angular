0d406f6c2b951b6ad6913698f83bd2971ca0617d
anhany <and.hanc@gmail.com>
2017-05-07 11:43:55 -0600

    first working connection. which made an entity by itself!

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,28 @@
+import "reflect-metadata";
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { NestFactory } from 'nest.js';
 import { ApplicationModule } from './app.module';
+
+// database connection
+import {createConnection} from 'typeorm';
+import {Photo} from './models/Photo';
+createConnection({
+    driver: {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: "typeorm",
+        password: "",
+        database: "typeorm"
+    },
+    entities: [
+        Photo
+    ],
+    autoSchemaSync: true
+}).then(connection => {
+    // here you can start to work with your entities
+}).catch(error => console.log(error));
 
 const instance = express();
 instance.use(bodyParser.json());
